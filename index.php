cb708aa0959208456ab2cb0c5cdac3f3dad12a39
RonaldoBarbosaAraujo <ronaldojp1039@gmail.com>
2024-05-14 17:02:46 -0300

    Tarefe de casa

--- index.php
+++ index.php
@@ -8,24 +8,117 @@
     
 </head>
 <body> 
-   <?php
-   $nome = "Ronaldo";
-   $sobrenome = "Barbosa";
-   ?>
+    <h1>Título em HTML</h1>
     
-   
-    <?php
-
-     function media($nota1, $nota2, $nota3, $nota4){
-          $mediafinal = ($nota1 + $nota2 + $nota3 + $nota4)/4;
-          return $mediafinal;
-        
-      }
-      echo (media(9, 5, 10, 10));
-    ?>
-    <h1>Ronaldo</h1>
-   
   
+    <?php
+        /*$carros = array("Opala", "Corolla", "Belina", "Dodge", "Hilux");
+        var_dump($carros);
+       
+        $cursos = ["Programador", "Maquiagem", "Cabelereiro", "Fotografia", "Direção Defensiva", "Macanico"];
+        var_dump($cursos);
+        echo "A Quantidade de valores do Array".count($cursos);
+        foreach($cursos as $profissão){
+            echo "$profissão <br>";
+        }*/
+
+       
+
+
+        /*$Calendario2024 = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];*/
+
+        //$Calendario2024[4] = "Agosto";
+       // echo $Calendario2024[4];
+    
+       
+
+            /*foreach($Calendario2024 as $mes){
+            echo "$mes <br>";
+        }*/
+
+
+
+        /*for ($x = 1; $x <= 15; $x++){
+            echo "$x <br>";
+        }*/
+
+        /*for ($x = 1; $x <= 10; $x++){
+            $resultado = $x*3;
+            echo "O resultado é $resultado <br>";
+        }*/
+
+        /*for ($x = 1; $x <= 10; $x++){
+            $resultado = $x*3;
+            echo "1 x $x = $resultado <br>";
+        }
+        echo "<br>";
+        for ($x = 1; $x <= 10; $x++){
+            $resultado = $x*3;
+            echo "6 x $x = $resultado <br>";
+        }*/
+
+        $x = 2;
+        $y = 3;
+        $soma = $x + $y;
+        echo $resultado = $soma* $x; 
+        echo "<br><br>";
+
+        $nota1 = 5;
+        $nota2 = 9;
+        $nota3 = 10;
+        $media = ($nota1 + $nota2 + $nota3)/3;
+        echo $media;
+        echo "<br><br>";
+
+        $x = 5;
+        $total = $x* 0.15;
+        echo $total;
+        echo "<br><br>";
+
+        $x = 10;
+        $total = $x* 0.05;
+        $total1 = $x* 0.5;
+        echo "O valor de 5% é = $total <br> E o valor de 50% é = $total1";
+        echo "<br><br>";
+
+        $x = 15;
+        if ($x >0){
+            echo "Valor Positivo";
+        }
+        else if($x <0){
+            echo "Valor Negativo";
+        } else{
+            echo "Igual a zero";
+        }
+        echo "<br><br>";
+
+        for ($x = 0; $x <= 10; $x++){
+            $resultado = $x*4;
+            echo "4 x $x = $resultado <br>";
+        }
+        echo "<br><br>"
+
+
+
+
+
+        
+
+        
+
+
+    
+
+
+        
+
+
+
+
+
+    ?> 
+    
+
 
    <scrip src= "JS/script.js"></script> 
 </body>
